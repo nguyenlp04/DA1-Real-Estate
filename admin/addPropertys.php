b842d69f99cf696e639cdc4cc2605f163c90f607
Nguyen Quang Cuong <nguyenquangcuong25022004@gmail.com>
2023-11-08 23:39:52 +0700

    test inc

---
+++ admin/addPropertys.php
@@ -0,0 +1,7 @@
+<?php include "../inc/header.php";?>
+
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">Thêm bất động sản</h1>
+</div>
+dfhdf
+<?php include "../inc/footer.php";?>
