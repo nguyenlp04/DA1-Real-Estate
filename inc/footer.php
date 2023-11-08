e9ec7ad4b70471d1fe4d2199af23f67574168341
Nguyen Quang Cuong <nguyenquangcuong25022004@gmail.com>
2023-11-09 00:14:05 +0700

    add footer and header

---
+++ inc/footer.php
@@ -0,0 +1,44 @@
+</div>
+<!-- /.container-fluid -->
+</div>
+<!-- End of Main Content -->
+
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Your Website 2021</span>
+        </div>
+    </div>
+</footer>
+<!-- End of Footer -->
+</div>
+<!-- End of Content Wrapper -->
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+<!-- Logout Modal-->
+
+<!-- Bootstrap core JavaScript-->
+<script src="../assets/js/dashboard/jquery.min.js"></script>
+<script src="../assets/js/dashboard/bootstrap.bundle.min.js"></script>
+<script src="../assets/js/dashboard/sb-admin-2.min.js"></script>
+<script src="../assets/js/dashboard/Chart.min.js"></script>
+<script src="../assets/js/dashboard/chart-pie-demo.js"></script>
+<script src="../assets/js/dashboard/chart-area-demo.js"></script>
+
+<!-- Core plugin JavaScript-->
+<!-- <script src="../assets/js/dashboard.jquery.easing.min.js"></script>
+    <script src="../assets/js/dashboard.bootstrap.min.js"></script>
+    
+     Custom scripts for all pages-->
+
+<!-- Page level plugins -->
+</body>
+
+</html>
