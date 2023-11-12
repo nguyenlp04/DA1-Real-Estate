d1b4878b69748a1baf5bb8885bcb9c2c4344906f
Lê Phước Nguyên <117273543+nguyenlp04@users.noreply.github.com>
2023-11-12 17:39:14 +0700

    Revert "PhuocNguyen pull request"

--- admin/inc/footerDashboard.php
+++ admin/inc/footerDashboard.php
@@ -1,26 +1,24 @@
 </div>
-<!-- End of Main Content -->
-</div>
-<!-- End of Content Wrapper -->
-</div>
-<!-- End of Page Wrapper -->
-<!-- Scroll to Top Button-->
-<a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
-</a>
+ <!-- End of Main Content -->
+ </div>
+ <!-- End of Content Wrapper -->
+ </div>
+ <!-- End of Page Wrapper -->
+ <!-- Scroll to Top Button-->
+ <a class="scroll-to-top rounded" href="#page-top">
+   <i class="fas fa-angle-up"></i>
+ </a>
 
-<!-- Bootstrap core JavaScript-->
+ <!-- Bootstrap core JavaScript-->
+ <script src="../assets/js/dashboard/jquery.min.js"></script>
+ <script src="../assets/js/dashboard/bootstrap.bundle.min.js"></script>
+ <script src="../assets/js/dashboard/sb-admin-2.min.js"></script>
+ <script src="../assets/js/dashboard/Chart.min.js"></script>
+ <script src="../assets/js/dashboard/chart-pie-demo.js"></script>
+ <script src="../assets/js/dashboard/chart-area-demo.js"></script>
 
-<script src="../assets/js/dashboard/jquery.min.js"></script>
-<script src="../assets/js/dashboard/bootstrap.bundle.min.js"></script>
-<script src="../assets/js/dashboard/sb-admin-2.min.js"></script>
-<script src="../assets/js/dashboard/Chart.min.js"></script>
-<script src="../assets/js/dashboard/chart-pie-demo.js"></script>
-<script src="../assets/js/dashboard/chart-area-demo.js"></script>
-
-<!-- Core plugin JavaScript-->
-<!-- <script src="../assets/js/dashboard.jquery.easing.min.js"></script>
+ <!-- Core plugin JavaScript-->
+ <!-- <script src="../assets/js/dashboard.jquery.easing.min.js"></script>
     <script src="../assets/js/dashboard.bootstrap.min.js"></script>-->
-</body>
-
-</html>
+ </body>
+ </html>
