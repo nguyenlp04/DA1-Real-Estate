f1416adacdfbde3e5933fb52c7a5fbe875af9039
Lê Phước Nguyên <lphuonguye.ecn.2182004@gmail.com>
2023-11-12 22:29:11 +0700

    Committing local changes before pull with rebase

--- admin/models/posts.php
+++ admin/models/posts.php
@@ -1,5 +1,20 @@
 <?php 
+class Database
+{
+    private  $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $dbname = "da1-real-estate";
+    public $conn;
+    public function __construct()
+    {
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->dbname);
 
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+}
 class Post
 {
     private $db;
