e786eed96ab0d290a978f4dfd824310432e28682
Lê Phước Nguyên <lphuonguye.ecn.2182004@gmail.com>
2023-11-22 08:28:01 +0700

    merge

--- admin/controllers/profile/profileDetail.php
+++ admin/controllers/profile/profileDetail.php
@@ -147,11 +147,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                                                 </div>
                                             </div>
-<<<<<<< HEAD
                                             <div class="flex-auto pl-4 mb-6">
-=======
-                                            <div class="flex-auto pl-4">
->>>>>>> PhuocNguyen-Auth
                                                 <p class="leading-normal text-sm">
                                                     Chào mừng bạn đến với trang quản trị hãy cố gắng mỗi ngày nhé !
                                                 </p>
@@ -166,11 +162,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                                     <li
                                                         class="relative block px-4 py-2 pl-0 leading-normal bg-white border-0 border-t-0 text-sm text-inherit">
                                                         <strong class="text-slate-700">Số điện thoại:</strong> &nbsp;
-<<<<<<< HEAD
                                                         +84<?php echo  $_SESSION['user_info']['phone_number'] ?>
-=======
-                                                        <?php echo  $_SESSION['user_info']['phone_number'] ?>
->>>>>>> PhuocNguyen-Auth
                                                     </li>
                                                     <li
                                                         class="relative block px-4 py-2 pl-0 leading-normal bg-white border-0 border-t-0 text-sm text-inherit">
@@ -263,9 +255,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                                             </div>
                                         </div>
-<<<<<<< HEAD
-
-=======
                                         <footer class="pt-4">
                                             <div class="w-full px-6 mx-auto">
                                                 <div class="flex flex-wrap items-center -mx-3 lg:justify-between">
@@ -314,7 +303,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                                 </div>
                                             </div>
                                         </footer>
->>>>>>> PhuocNguyen-Auth
                                     </div>
                                 </div>
                                 <div class="flex flex-wrap -mx-3 hidden section" id="editInfo">
