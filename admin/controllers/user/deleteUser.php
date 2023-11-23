952fdc7ee529e85c47c2b077229fec5e0008fb7d
Lê Phước Nguyên <lphuonguye.ecn.2182004@gmail.com>
2023-11-23 10:36:29 +0700

    update config dele

--- admin/controllers/user/deleteUser.php
+++ admin/controllers/user/deleteUser.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 include(__DIR__ . '/../../models/user.php');
-
+include(__DIR__ . '/../../../config/config.php');
 $iduser = $_GET['user_id'];
  $database = new Database();
  $User = new User($database);
